5c5f850584c41939f739d4b98d1e7a8f7a598480
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-27 17:13:17 +0900

    フォルダー名を変更

--- neoboot.c
+++ neoboot.c
@@ -1,11 +1,11 @@
 #include <efi.h>
 #include <efilib.h>
-#include "proto/common.h"
+#include "inc/common.h"
 
 
 // Main 
 EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *SystemTable) {
-    
+
     // Initalize EFI
     InitializeLib(IH, SystemTable);
 
@@ -13,7 +13,8 @@ EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *SystemTable) {
     EFI_FILE_PROTOCOL *root_dir;
     open_root_dir(IH, root_dir);
 
-    // 
+    // Init Memory Map
+    
 
 
 
