7d9d7b5624d0e83db473bfde5ce12aad4efa5dcd
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-27 18:39:10 +0900

    メモリーマップを保存できるようにした

---
+++ src/main.c
@@ -0,0 +1,93 @@
+#include <efi.h>
+#include <efilib.h>
+
+#include "memory.h"
+
+// AsciiSPrint
+UINTN EFIAPI AsciiSPrint(CHAR8 *buffer, UINTN buffer_size, CONST CHAR8 *str, ...) {
+    va_list marker;
+    UINTN num_printed;
+
+    va_start(marker, str);
+    num_printed = AsciiVSPrint(buffer, buffer_size, str, marker);
+    va_end(marker);
+    return num_printed;
+}
+
+
+// Get memory type
+const CHAR16 *get_memtype(EFI_MEMORY_TYPE type) {
+
+    switch (type) {
+        case EfiReservedMemoryType: return L"EfiReservedMemoryType";
+        case EfiLoaderCode: return L"EfiLoaderCode";
+        case EfiLoaderData: return L"EfiLoaderData";
+        case EfiBootServicesCode: return L"EfiBootServicesCode";
+        case EfiBootServicesData: return L"EfiBootServicesData";
+        case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+        case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+        case EfiConventionalMemory: return L"EfiConventionalMemory";
+        case EfiUnusableMemory: return L"EfiUnusableMemory";
+        case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+        case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+        case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+        case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+        case EfiPalCode: return L"EfiPalCode";
+        case EfiPersistentMemory: return L"EfiPersistentMemory";
+        case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+        default: return L"InvalidMemoryType";
+    }
+
+}
+
+// Save memory map file
+EFI_STATUS save_memmap(memmap *map, EFI_FILE_PROTOCOL *f, EFI_FILE_PROTOCOL *esp_root) {
+    char buffer[256];
+    EFI_STATUS status;
+    UINTN size;
+
+    // Header
+    CHAR8 *header = "Index, Buffer, Type, Type(name), PhysicalStart, VirtualStart, NumberOfPages, Size, Attribute\n"
+                    "-----|------------------|----|----------------------|------------------|------------------|------------------|-----|----------------|\n";
+    size = strlena(header);
+
+    // Create a file
+    status = uefi_call_wrapper(esp_root->Open, 5, esp_root, &f, L"\\neoboot\\memmap.blmm", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    ASSERT(!EFI_ERROR(status))
+
+    // Write header
+    status = uefi_call_wrapper(f->Write, 3, f, &size, header);
+    ASSERT(!EFI_ERROR(status));
+
+    // Write memory map
+    for (UINTN i = 0; i < map->entry; i++) {
+        EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)((char *)map->buffer + map->desc_size * i);
+        size = AsciiSPrint(buffer, sizeof(buffer), "| %02u | %016x | %02x | %20ls | %016x | %016x | %016x | %3d | %2ls %5lx | \n", i, desc, desc->Type, get_memtype(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages, (desc->Attribute & EFI_MEMORY_RUNTIME) ? L"RT" : L"", desc->Attribute & 0xffffflu);
+
+        status = uefi_call_wrapper(f->Write, 3, f, &size, buffer);
+        ASSERT(!EFI_ERROR(status));
+    }
+
+    // Close file handle
+    uefi_call_wrapper(f->Close, 1, f);
+
+    return EFI_SUCCESS;
+}
+
+
+
+EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *ST) {
+    // Initalize
+    InitializeLib(IH, ST);
+
+    // Get memory map
+    memmap map;
+    map.buffer = LibMemoryMap(&map.entry, &map.map_key, &map.desc_size, &map.desc_ver);
+    ASSERT(map.buffer != NULL);
+
+    // Save memory map
+
+
+
+
+}
