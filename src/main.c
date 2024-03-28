53403c278c8c2b3c7642e6e3964f0629d0e02f76
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-28 23:13:52 +0900

    コードの整形

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@ UINTN EFIAPI AsciiSPrint(CHAR8 *buffer, UINTN buffer_size, CONST CHAR8 *str, ...
     return num_printed;
 }
 
-
 // Get memory type
 const CHAR16 *get_memtype(EFI_MEMORY_TYPE type) {
 
@@ -90,53 +89,39 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     ST = SystemTable;
     BS = SystemTable->BootServices;
     RT = SystemTable->RuntimeServices;
-    Print(L"STEP0 \n");
 
     // Open LIP
     EFI_LOADED_IMAGE_PROTOCOL *lip = NULL;
     EFI_GUID lip_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
 
     open_protocol(ImageHandle, &lip_guid, (void **)&lip, ImageHandle, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-    Print(L"STEP1 \n");
 
     // Open ESP Root
     EFI_FILE_HANDLE esp_root;
     esp_root = LibOpenRoot(lip->DeviceHandle);
-    Print(L"STEP2 \n");
 
     // Get memory map
     memmap map;
     map.buffer = LibMemoryMap(&map.entry, &map.map_key, &map.desc_size, &map.desc_ver);
     ASSERT(map.buffer != NULL);
-    Print(L"STEP3 \n");
 
     // Save memory map
 
     EFI_FILE_PROTOCOL *memmap_file = NULL;
     save_memmap(&map, memmap_file, esp_root);
-    Print(L"STEP4 \n");
-    
-    // // Print Memory map
-    // for (UINTN i = 0; i < map.entry; i++) {
-    //     EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)((char *)map.buffer + map.desc_size * i);
-    //     Print(L"| %02u | %016x | %02x | %20ls | %016x | %016x | %016x | %3d | %2ls %5lx | \n", i, desc, desc->Type, get_memtype(desc->Type), desc->PhysicalStart, desc->VirtualStart, desc->NumberOfPages,desc->NumberOfPages , (desc->Attribute & EFI_MEMORY_RUNTIME) ? L"RT" : L"", desc->Attribute & 0xffffflu);
-    // }
 
     // Free up of memory
     FreePool(map.buffer);
-    Print(L"STEP5 \n");
 
     // Open Block IO Protocol
     EFI_BLOCK_IO *block_io;
     EFI_GUID block_io_guid = EFI_BLOCK_IO_PROTOCOL_GUID;
     open_protocol(lip->DeviceHandle, &block_io_guid, (VOID **)&block_io, ImageHandle, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-    Print(L"STEP6 \n");
 
     // Get partitions info from block devices
     EFI_BLOCK_IO_MEDIA *block_io_media = block_io->Media;
     UINTN num_blocks = block_io_media->LastBlock + 1;
     Print(L"Block Info : Blocks : %x\n", num_blocks);
-    Print(L"STEP7 \n");
 
     while(1) __asm__ ("hlt");
 }
