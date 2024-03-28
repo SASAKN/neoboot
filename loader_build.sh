f558edba951699eb64d223f36c6127a11a40c6ff
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-03-28 22:42:53 +0900

    クロスコンパイラを立ち上げた

---
+++ loader_build.sh
@@ -0,0 +1,3 @@
+x86_64-elf-gcc -Ignu-efi/inc -fpic -ffreestanding -fno-stack-protector -fno-stack-check -fshort-wchar -mno-red-zone -maccumulate-outgoing-args -c src/main.c -o main.o
+x86_64-elf-ld -shared -Bsymbolic -Lgnu-efi/x86_64/lib -Lgnu-efi/x86_64/gnuefi -Tgnu-efi/gnuefi/elf_x86_64_efi.lds gnu-efi/x86_64/gnuefi/crt0-efi-x86_64.o main.o -o main.so -lgnuefi -lefi
+x86_64-elf-objcopy -j .text -j .sdata -j .data -j .rodata -j .dynamic -j .dynsym  -j .rel -j .rela -j .rel.* -j .rela.* -j .reloc --target efi-app-x86_64 --subsystem=10 main.so main.efi
