c894795d990065d709a2d81a9c3f05f40385259e
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-05-24 18:20:48 +0900

    GUI表示に対応

--- loader_build.sh
+++ loader_build.sh
@@ -75,7 +75,7 @@ function run_image() {
     -drive if=ide,index=0,media=disk,format=raw,file=${IMAGE_PATH} \
     -device nec-usb-xhci,id=xhci \
     -device usb-mouse -device usb-kbd \
-    -nographic
+    -monitor mon:stdio
 }
 
 #クリーン
