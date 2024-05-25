50f5f45525f431b99a2e98c31751ece2d18376c0
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2024-05-25 19:34:22 +0900

    新しく作る

--- src/new.c
+++ src/new.c
@@ -1,7 +1,9 @@
+// GNU_EFI
 #include <efi.h>
 #include <efilib.h>
 #include <efigpt.h>
 
+// NEOBOOT
 #include "memory.h"
 #include "config.h"
 
@@ -14,54 +16,6 @@ UINTN EFIAPI AsciiSPrint(CHAR8 *buffer, UINTN buffer_size, CONST CHAR8 *str, ...
     num_printed = AsciiVSPrint(buffer, buffer_size, str, marker);
     va_end(marker);
     return num_printed;
-}
-
-// UEFI Device Path Lib Locate Protocol
-VOID *uefi_dev_path_lib_locate_protocol (EFI_GUID *p_guid) {
-    EFI_STATUS status;
-    VOID *p;
-
-    status = uefi_call_wrapper(BS->LocateProtocol, 3, p_guid, NULL, (VOID **)&p);
-    if (EFI_ERROR(status)) {
-        return NULL;
-    } else {
-        return p;
-    }
-}
-
-// Convert Device Path To Text
-EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *dev_path_lib_to_str = NULL;
-CHAR16* EFIAPI ConvertDevicePathToText(CONST EFI_DEVICE_PATH_PROTOCOL *dev_path, BOOLEAN display_only, BOOLEAN allow_shortcuts) {
-    if (dev_path_lib_to_str == NULL) {
-        dev_path_lib_to_str = uefi_dev_path_lib_locate_protocol(&gEfiDevicePathToTextProtocolGuid);
-    }
-
-    if (dev_path_lib_to_str != NULL) {
-        return (CHAR16 *)uefi_call_wrapper(dev_path_lib_to_str->ConvertDevicePathToText, 3, dev_path, display_only, allow_shortcuts);
-    } else {
-        return NULL;
-    }
-}
-
-// Read GPT PARTITION ENTRY
-EFI_GUID gEfiPartTypeSystemPartitionGuid = { 0xC12A7328L, 0xF81F, 0x11D2, {0xBA, 0x4B, 0x00, 0xA0, 0xC9, 0x3E, 0xC9, 0x3B }};
-EFI_GUID gEfiPartTypeBasicDataGuid = { 0xEBD0A0A2L, 0xB9E5, 0x4433, {0x87, 0xC0, 0x68, 0xB6, 0xB7, 0x26, 0x99, 0xC7 }};
-EFI_STATUS ReadGptPartitionEntry(EFI_BLOCK_IO_PROTOCOL *BlockIo, UINT32 PartitionIndex, EFI_PARTITION_ENTRY **PartEntry) {
-    EFI_STATUS Status;
-    UINT32 BlockSize = BlockIo->Media->BlockSize;
-    UINT64 Lba = 1 + PartitionIndex * sizeof(EFI_PARTITION_ENTRY) / BlockSize;
-
-    *PartEntry = AllocatePool(sizeof(EFI_PARTITION_ENTRY));
-    if (*PartEntry == NULL) {
-        return EFI_OUT_OF_RESOURCES;
-    }
-
-    Status = BlockIo->ReadBlocks(BlockIo, BlockIo->Media->MediaId, Lba, sizeof(EFI_PARTITION_ENTRY), (VOID *)*PartEntry);
-    if (EFI_ERROR(Status)) {
-        FreePool(*PartEntry);
-    }
-
-    return Status;
 }
 
 // Get memory type
